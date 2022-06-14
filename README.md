7ff9b67f5b46ff1841947ea1e42a596ef61f58b2
naeminhye <minhhieu.eng2@gmail.com>
2022-06-14 22:17:46 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# daisyui-react-cra-qy3s7f
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/daisyui-react-cra-qy3s7f)
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  plugins: [require('daisyui')],
+  // daisyUI config (optional)
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: '',
+    darkTheme: 'bumblebee',
+  },
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+  },
+};
